88ab8452dd7e96799e1fcde36661f5cbdcc3a891
Pedro Jardel Zago Lima <pedrozagolima@gmail.com>
2024-06-15 06:26:00 -0300

    Utilizando cookies no Fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 import { knex } from '../database'
 import { randomUUID } from 'crypto'
 
+// Coockies <-> Formas da gente manter contexto entre requisições
+
 export async function transactionsRoutes(server: FastifyInstance) {
   server.get('/', async () => {
     const transactions = await knex('transactions').select()
@@ -36,10 +38,18 @@ export async function transactionsRoutes(server: FastifyInstance) {
 
     const { title, amount, type } = createTransactionBodySchema.parse(req.body)
 
+    const sessionId = req.cookies.sessionId ?? randomUUID()
+
+    res.cookie('sessionId', sessionId, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return res.status(201).send()
